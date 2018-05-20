4e2ff262405bd6ffc73d7521de4f0150dc908941
Bhupesh Patel <bhupesh.patel@salesforce.com>
2018-05-20 11:10:41 -0700

    Adding event timer countdown

--- scs-express-app/src/components/EventCard.js
+++ scs-express-app/src/components/EventCard.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import _ from 'underscore';
-import moment from 'moment';
-import 'moment-timezone';
+import {getDateFromStr, getMonthFromDate, getDayFromDate, getEventCountDown} from '../utils/datetimeutil';
 import { Link } from "react-router-dom";
+
 
 const url = "http://52.191.119.42:1337"
 
@@ -13,6 +13,7 @@ class EventCard extends Component {
         var day = ""
         var month = ""
         var imageUrl = ""
+        var dateCountDownStr = ""
     
         if(_.isEmpty(this.props.event.StartDate)) {
             return <div />
@@ -24,9 +25,11 @@ class EventCard extends Component {
         }
         
         if(!_.isEmpty(this.props.event.StartDate)) {
-            eventDate = moment(this.props.event.StartDate).format('YYYY/MM/DD')
-            day =  moment(this.props.event.StartDate).format('D');
-            month =  moment(this.props.event.StartDate).format('MMM');
+            eventDate = getDateFromStr(this.props.event.StartDate)
+            day =  getDayFromDate(this.props.event.StartDate);
+            month =  getMonthFromDate(this.props.event.StartDate);
+            dateCountDownStr = getEventCountDown(this.props.event.StartDate);
+            //dateCountDownStr = getEventCountDown("2018-05-22T18:00:00.000Z");
         }
         
         return (
@@ -43,7 +46,7 @@ class EventCard extends Component {
             </div>
             <div className="col-md-7">
                 <div className="xs-event-content">
-                    <Link to={{ pathname: `/events/${this.props.event.id}`,
+                    <Link to={{ pathname: `/events/${this.props.event.ID}`,
                                     state: {
                                         event: this.props.event,
                                         eventDate: eventDate,
@@ -51,10 +54,9 @@ class EventCard extends Component {
                                         eventMonth: month
                                     } }}>{this.props.event.Title}</Link>
                     <p>{this.props.event.ShortDescription}</p>
-                    <div className="xs-countdown-timer" data-countdown="2018/05/20">
-                        <span className="timer-count">{moment(eventDate).format('DD')}<span className="timer-title">Days</span></span> 
-                        <span className="timer-count">{moment(eventDate).format('hh')}<span className="timer-title">Hours</span></span>
-                        <span className="timer-count">{moment(eventDate).format('mm')}<span className="timer-title">Minutes</span></span>
+                    <div className="xs-countdown-timer" data-countdown={eventDate}>
+                        <span className="timer-count">{dateCountDownStr.split(':')[0]}<span className="timer-title">Days</span></span> 
+                        <span className="timer-count">{dateCountDownStr.split(':')[1]}<span className="timer-title">Hours</span></span>
                     </div>
                     <Link to={{ pathname: `/events/${this.props.event.ID}`}} className="btn btn-primary">Learn More</Link>
                 </div>
--- scs-express-app/src/components/Events.js
+++ scs-express-app/src/components/Events.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import EventCard from './EventCard';
 import eventBackgroundImg from './images/event_bg.jpg';
+import {getEventCountDown} from '../utils/datetimeutil';
+import _ from 'underscore';
 import axios from 'axios';
 //import seniorCitizenLogo from './images/SeniorCenter-logo.jpg';
 //import womanEmpowerLogo from './images/woman-empower.jpg';
@@ -20,13 +22,30 @@ class Events extends Component {
 		axios.get(`${url}/events?_sort=StartTime:desc`)
 			.then(res => {
 				const events = res.data;
-				this.setState({ events })
+				// const filteredEvents = _.filter(events, function(event) {
+				// 	var eventStartDate = getEventCountDown(event.StartDate);
+				// 	if (!_.isEmpty(eventStartDate)) {
+				// 		if (eventStartDate.split(":")[0] > 0) {
+				// 			return event;
+				// 		}
+				// 	}
+				// });
+				//console.log("Filtered Events from strapi: ", events)
+				// if (filteredEvents.length > 0) {
+				// 	this.setState({ filteredEvents })
+				// }
+				this.setState({ events });
 			});
-			//console.log("Events from strapi: ", this.state.events)
 	}
 
     render(){
+		const eventCards = this.state.events.length > 0 ? (
+			this.state.events.map(e => <EventCard  key={e._id} event={e} />)
+		) : (
+			<h2>No upcoming events!!</h2>	
+		)
         return (
+		
 		<div>
 			<section className="xs-banner-inner-section parallax-window" style={{backgroundImage: `url(${eventBackgroundImg})`}}>
                 <div className="xs-black-overlay"></div>
@@ -41,7 +60,7 @@ class Events extends Component {
 				<section className="xs-content-section-padding">
 					<div className="container">
 						<div className="row">
-							{this.state.events.map(e => <EventCard  key={e._id} event={e} />)}
+							{eventCards}
 						</div>
 					</div>
 				</section>
