9cd07853091ab65475e4d061752c0d0b39664f32
bhupesh.patel <bhupesh.patel@salesforce.com>
2018-10-15 13:45:53 -0700

    Fixing carousel

--- scs-express-app/public/assets/js/main.js
+++ scs-express-app/public/assets/js/main.js
@@ -175,25 +175,23 @@ $(document).ready(function() {
 		});
 	}
 	
-	$('.customer-logos').slick({
-		slidesToShow: 6,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 1000,
-		arrows: false,
-		dots: false,
-		pauseOnHover: false,
-		responsive: [{
-		  breakpoint: 768,
-		  settings: {
-			slidesToShow: 4
-		  }
-		}, {
-		  breakpoint: 520,
-		  settings: {
-			slidesToShow: 3
-		  }
-		}]
+	$('#myCarousel').carousel({
+		interval: 3000
+	  })
+	  
+	  $('.carousel .carousel-item').each(function(){
+		var next = $(this).next();
+		if (!next.length) {
+		  next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
+		
+		if (next.next().length>0) {
+		  next.next().children(':first-child').clone().appendTo($(this));
+		}
+		else {
+			$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+		}
 	  });
 
 	/*==========================================================
